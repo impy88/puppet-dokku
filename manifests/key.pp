123c15900e8369c8cf9c9bd2c7d56965affef288
Maxim Kotelnikov <info@impy.us>
2016-02-24 15:57:17 +0300

    Key add

--- manifests/key.pp
+++ manifests/key.pp
@@ -15,7 +15,7 @@ define dokku::key (
 ) {
 
   exec { "dokku key add ${name}":
-    command => "/bin/echo ${key} | /usr/local/bin/sshcommand acl-add dokku User ${name}",
+    command => "/bin/echo ${key} | /usr/local/bin/sshcommand acl-add dokku 'User ${name}'",
     unless  => "/bin/grep -c ${name}$ ${::dokku::params::data_dir}/.ssh/authorized_keys",
     require => Class['dokku::install'],
   }
